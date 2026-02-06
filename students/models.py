c281248da556358e5db78c9d3c92ca90464de3c7
Mohamed <mohamedoulkadi03@gmail.com>
2026-02-06 13:11:21 +0100

    feat: Implement Employee model with API endpoints and enhance existing Student API functionality.

--- students/models.py
+++ students/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    student_id = models.IntegerField(max_length=10)
+    student_id = models.IntegerField()
     name = models.CharField(max_length=20)
     branch = models.CharField(max_length=50)
 
